111a5c4b8fa1a97fa9508938946645e017ad442a
Haezebrouck Jules <jules.haezebrouck@uzgent.be>
2022-01-26 17:14:50 +0100

    remove request handler

--- manifests/init.pp
+++ manifests/init.pp
@@ -147,8 +147,19 @@ class acme (
         }
       }
 
-      class { '::acme::request::handler' :
-        require => Class[::acme::setup],
+      # needed for the openssl ocsp -header flag
+      $old_openssl = versioncmp($::openssl_version, '1.1.0') < 0
+
+      file { $acme::ocsp_request:
+        ensure  => file,
+        owner   => 'root',
+        group   => $acme::group,
+        mode    => '0755',
+        content => epp("${module_name}/get_certificate_ocsp.sh.epp", {
+          old_openssl => $old_openssl,
+          path        => $acme::path,
+          proxy       => $acme::proxy,
+          }),
       }
     }
     # Collect certificates.
